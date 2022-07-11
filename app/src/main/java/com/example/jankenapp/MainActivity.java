06207b9c494af3ab65a6b9e7fda871230cda83a6
Tyarimon <tyarinko239@icloud.com>
2022-07-11 14:03:32 +0900

    連携確認

--- app/src/main/java/com/example/jankenapp/MainActivity.java
+++ app/src/main/java/com/example/jankenapp/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /*お試だお*/
         /*無印良*/
-        /*これ見えてる*/
+        /*これ見えてる？*/
     }
 }
