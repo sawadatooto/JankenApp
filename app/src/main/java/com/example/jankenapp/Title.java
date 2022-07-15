08dc17b83fce1345b2969785a60520c57614b827
20205063 <20205063@g.aut.ac.jp>
2022-07-15 14:08:55 +0900

    あ

--- app/src/main/java/com/example/jankenapp/Title.java
+++ app/src/main/java/com/example/jankenapp/Title.java
@@ -2,8 +2,10 @@ package com.example.jankenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Title extends AppCompatActivity {
 
@@ -13,5 +15,8 @@ public class Title extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-
+    public void onStartButton(View view){
+        Intent intent = new Intent(this, Difficulty.class);
+        startActivity(intent);
+    }
 }
