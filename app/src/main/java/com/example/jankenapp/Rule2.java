e9585044414e40e5b43e054be26c90e11bea5602
20205063 <20205063@g.aut.ac.jp>
2022-07-18 16:03:46 +0900

    ルール２作成

--- app/src/main/java/com/example/jankenapp/Rule2.java
+++ app/src/main/java/com/example/jankenapp/Rule2.java
@@ -1,7 +1,6 @@
 package com.example.jankenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +8,9 @@ import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 
-public class Rule extends AppCompatActivity {
+
+public class Rule2 extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
