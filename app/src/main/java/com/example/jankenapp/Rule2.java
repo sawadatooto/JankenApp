f40b4ad4a3e3e7cd3beb5a537d80a91e8995e53b
Tyarimon <tyarinko239@icloud.com>
2022-07-22 02:24:55 +0900

    説明画面完成

--- app/src/main/java/com/example/jankenapp/Rule2.java
+++ app/src/main/java/com/example/jankenapp/Rule2.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 
 public class Rule2 extends AppCompatActivity {
@@ -14,23 +15,51 @@ public class Rule2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rule);
-        ruleImageSwitcher = findViewById(R.id.ruleView);
-        ruleImageSwitcher.setFactory(() -> new ImageView(getApplicationContext()));
-        Button tapButton = findViewById(R.id.nextButton);
-        tapButton.setOnClickListener(v -> movePosition());
+        setContentView(R.layout.activity_rule2);
+        ruleImageSwitcher = (ImageSwitcher) findViewById(R.id.ruleView);
+        ruleImageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getApplicationContext());
+                imageView.setImageResource(R.drawable.rule0);
+                return imageView;
+            }
+        });
+        Button nextButton = (Button) findViewById(R.id.nextButton);
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setVisibility(View.INVISIBLE);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                backButton.setVisibility(View.VISIBLE);
+                movePosition(1);
+                if (ruleSlide == ruleImageResources.length - 1) {
+                    nextButton.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextButton.setVisibility(View.VISIBLE);
+                movePosition(-1);
+                if (ruleSlide == 0) {
+                    backButton.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 
     ImageSwitcher ruleImageSwitcher;
     int[] ruleImageResources = { R.drawable.rule0, R.drawable.rule1, R.drawable.rule2};
 
     int ruleSlide = 0;
-    private void movePosition() {
-        ruleSlide = ruleSlide + 1;
+    private void movePosition(int move) {
+        ruleSlide = ruleSlide + move;
         if (ruleSlide >= ruleImageResources.length) {
-            ruleSlide = 0;
+            ruleSlide = 2;
         } else if (ruleSlide < 0) {
-            ruleSlide = ruleImageResources.length -1;
+            ruleSlide = 0;
         }
         ruleImageSwitcher.setImageResource(ruleImageResources[ruleSlide]);
     }
